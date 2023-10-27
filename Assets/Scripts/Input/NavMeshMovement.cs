6ba04550b21e7a4ef2bfca7e2d8e0322856fcb9f
Shinkey <ilgammakh@mail.ru>
2023-10-28 00:01:42 +0300

    promej commit

--- Assets/Scripts/Input/NavMeshMovement.cs
+++ Assets/Scripts/Input/NavMeshMovement.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+using Camera;
 
 namespace Input
 {
@@ -16,10 +17,22 @@ namespace Input
 			_sprintSpeed = sprintSpeed;
 		}
 
+		private void Test()
+		{
+			float horizontal = CameraController.GetXAxis();
+			float vertical = CameraController.GetYAxis();
+			Debug.Log(horizontal + " " + vertical);
+			//
+			// // Вычисляем направление движения относительно камеры
+			// Vector3 cameraForward = Vector3.Scale(freeLookCam.transform.forward, new Vector3(1, 0, 1)).normalized;
+			// Vector3 moveDirection = cameraForward * vertical + freeLookCam.transform.right * horizontal;
+		}
+		
 		protected override IEnumerator Move()
 		{
 			while (IsMove)
 			{
+				Test();
 				_navMeshAgent.Move( IsSprint ?
 					MoveDirection * (_sprintSpeed * Time.deltaTime) :
 					MoveDirection * Time.deltaTime);
