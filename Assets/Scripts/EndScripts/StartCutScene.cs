5fed0f8acf343fc395390f8674057a6bbde86d83
hloki <korpusprime@gmail.com>
2023-10-30 16:42:32 +0300

    cut scene musor

--- Assets/Scripts/EndScripts/StartCutScene.cs
+++ Assets/Scripts/EndScripts/StartCutScene.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using BaseClasses;
+using Input;
 using UI;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -8,21 +10,35 @@ using UnityEngine.Playables;
 public class StartCutScene : MonoBehaviour
 {
     [SerializeField] private GameObject leg;
+    [SerializeField] private GameObject mainvirt;
 
 
+    private void OnEnable()
+    {
+        GameStateEvents.GameWin += StartPlay;
+    }
 
 
     private void Start()
     {
-        CustomBehaviour.GetCharacterController().gameObject.SetActive(false);
-        leg?.SetActive(true);
+        
+    }
+
+    private void Update()
+    {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space)) StartPlay();
     }
 
     private void StartPlay()
     {
+        InputManager.DisableActions();
+        mainvirt.gameObject.SetActive(false);
+        CustomBehaviour.GetCharacterController().gameObject.SetActive(false);
+        leg?.SetActive(true);
         CustomBehaviour.GetCharacterController().gameObject.SetActive(false);
         FindObjectOfType<UIManager>().gameObject.SetActive(false);
         PlayableDirector play = GetComponent<PlayableDirector>();
+        Debug.Log("play:" +play);
         play.Play();
     }
 }
